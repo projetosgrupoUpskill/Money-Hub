aa6d46554baa49651f2e5310c11bae08f7f61ee9
Natália Pinho <nati.pinho@gmail.com>
2026-04-09 15:33:17 +0100

    contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+
+const contacts = [
+    {id:1 , 
+	profileImg: "https://avatars.githubusercontent.com/u/168200274?v=4",
+	name: "Natália Carvalho de Pinho Joaquim",
+        studentNumber: 219,
+        gitHubLink: "https://github.com/natipinho"
+    },
+        {id:2 , 
+	profileImg: "https://avatars.githubusercontent.com/u/203020275?v=4",
+	name: "Rebeca Luiza Soares Cerqueira",
+        studentNumber: 224,
+        gitHubLink: "https://github.com/Rebeca-Soares"
+    }
+]
+
+function ContactCard () {
+const contactDetails = contacts.map (contact => 
+( <div key={contact.id}>
+<b>{contact.name}</b>
+<br />
+<p>Student Number:{contact.studentNumber}</p>
+<p>{contact.gitHubLink}</p>
+</div>
+));
+return contactDetails;
+};
+
+export default ContactCard
