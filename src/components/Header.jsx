936cfcb8ddd59107f31edd36355ddbafa85076f5
Rebeca Soares <rebecasoaresc.eng@gmail.com>
2026-04-09 17:20:59 +0100

    Add initial code

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import styles from "./Header.module.css"
+import money_hub_logo from "../assets/money_hub_logo.svg" 
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            {/* Ícone */}
+            <div className={styles.logo}>
+                <img src={money_hub_logo} alt="Money Hub" className={styles.logoImage} />
+            </div>
+
+            {/* Links */}
+            <nav className={styles.nav}>
+                <Link to="/" className={styles.navLink}>Home</Link>
+                <Link to="/about" className={styles.navLink}>Sobre</Link>
+                <Link to="/contact" className={styles.navLink}>Contato</Link>
+            </nav>
+        </header>
+    )
+}
